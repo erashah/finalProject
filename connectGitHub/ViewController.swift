14ee369da85d359f8b36cfc24a094ed125a3a079
Era Shah <erashah001@gmail.com>
2022-08-01 15:28:56 -0400

    hi hello

--- connectGitHub/ViewController.swift
+++ connectGitHub/ViewController.swift
@@ -12,8 +12,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        
+        //hello world!!
     }
-
+    var swing = 1; //random variable
+//test
 
 }
 
