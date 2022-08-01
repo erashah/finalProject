59cb5d9de97cab7cc2ee4416c375c37cca338843
Era Shah <erashah001@gmail.com>
2022-08-01 15:29:35 -0400

    delete random notes

--- connectGitHub/ViewController.swift
+++ connectGitHub/ViewController.swift
@@ -12,12 +12,8 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-        
-        
-        //hello world!!
     }
-    var swing = 1; //random variable
-//test
+
 
 }
 
